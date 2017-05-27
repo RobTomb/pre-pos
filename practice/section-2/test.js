bb2c978c60dcff04667c8890bfea480f5b4deaf9
robtomb <robtomb@outlook.com>
2017-05-27 16:57:07 +0800

    hehe

---
+++ practice/section-2/test.js
@@ -0,0 +1,22 @@
+'use strict';
+ const str = [
+    'a', 'a', 'a',
+    'e', 'e', 'e', 'e', 'e', 'e', 'e',
+    'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
+    't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
+    'g', 'g', 'g', 'g', 'g', 'g', 'g',
+    'b', 'b', 'b', 'b', 'b', 'b',
+    'd', 'd', 'd', 'd', 'd'
+  ];
+let wordslist = {};
+let words = "";
+str.forEach(function(item){
+    if( item in wordslist )
+        wordslist[item] += 1;
+    else{
+        wordslist[item] = 1;
+    }
+});
+console.log(wordslist);
